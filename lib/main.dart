1f6658be5f00f6be68b302db1fec277948265b02
PaHuHo <hoang.phan804@gmail.com>
2021-10-27 21:17:56 +0700

    update giao dien

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,17 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const MyApp());
 }
-
+Container _buildOutlinedButton(String text,Color color,{double width=50}) {
+  return Container(margin:EdgeInsets.fromLTRB(0,5,10,0), child:OutlinedButton(
+      onPressed: () {},
+      child: Text(text,style: TextStyle(color: Colors.white)),
+      style: OutlinedButton.styleFrom(
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(50.0)),
+          minimumSize: Size(width, 50),
+          backgroundColor: color,)));
+          
+}
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
@@ -24,6 +34,7 @@ class MyApp extends StatelessWidget {
               Container(
                 alignment: Alignment.bottomRight,
                 padding: EdgeInsets.fromLTRB(0, 0, 10, 5),
+                margin: const EdgeInsets.only(top: 10),
                 width: 215,
                 height: 150,
                 child: Text("0",style: TextStyle(fontSize: 20),),
@@ -31,100 +42,45 @@ class MyApp extends StatelessWidget {
               Row(
                 mainAxisSize : MainAxisSize.min,
                 children: [
-                  OutlinedButton(
-                    onPressed: (){}, 
-                    child: Text("AC",style: TextStyle(color: Colors.white),),
-                    style: OutlinedButton.styleFrom(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(90.0),
-                      ),
-                      backgroundColor: Colors.grey,
-                    ),
-                  ),
-                  OutlinedButton(onPressed: (){}, child: Text("C")),
-                  OutlinedButton(onPressed: (){}, child: Text("%")),
-                  OutlinedButton(
-                    onPressed: (){}, 
-                    child: Text("/",style: TextStyle(color: Colors.white),),
-                    style: OutlinedButton.styleFrom(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(90.0),
-                      ),
-                      backgroundColor: Colors.yellow,
-                    ),
-                  ),
+                  _buildOutlinedButton("AC",Colors.green),
+                  _buildOutlinedButton("C",Colors.green),
+                  _buildOutlinedButton("%",Colors.green),
+                  _buildOutlinedButton("/",Colors.blue),
                 ],
               ),
               Row(
                 mainAxisSize : MainAxisSize.min,
                 children: [
-                  OutlinedButton(onPressed: (){}, child: Text("7")),
-                  OutlinedButton(onPressed: (){}, child: Text("8")),
-                  OutlinedButton(onPressed: (){}, child: Text("9")),
-                  OutlinedButton(
-                    onPressed: (){}, 
-                    child: Text("*",style: TextStyle(color: Colors.white),),
-                    style: OutlinedButton.styleFrom(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(90.0),
-                      ),
-                      backgroundColor: Colors.yellow,
-                    ),
-                  ),
+                 _buildOutlinedButton("7",Colors.grey),
+                  _buildOutlinedButton("8",Colors.grey),
+                  _buildOutlinedButton("9",Colors.grey),
+                  _buildOutlinedButton("*",Colors.blue),
                 ],
               ),
               Row(
                 mainAxisSize : MainAxisSize.min,
                 children: [
-                  OutlinedButton(onPressed: (){}, child: Text("4")),
-                  OutlinedButton(onPressed: (){}, child: Text("5")),
-                  OutlinedButton(onPressed: (){}, child: Text("6")),
-                  OutlinedButton(
-                    onPressed: (){}, 
-                    child: Text("-",style: TextStyle(color: Colors.white),),
-                    style: OutlinedButton.styleFrom(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(90.0),
-                      ),
-                      backgroundColor: Colors.yellow,
-                    ),
-                  ),
+                  _buildOutlinedButton("4",Colors.grey),
+                  _buildOutlinedButton("5",Colors.grey),
+                  _buildOutlinedButton("6",Colors.grey),
+                  _buildOutlinedButton("-",Colors.blue),
                 ],
               ),
               Row(
                 mainAxisSize : MainAxisSize.min,
                 children: [
-                  OutlinedButton(onPressed: (){}, child: Text("1")),
-                  OutlinedButton(onPressed: (){}, child: Text("2")),
-                  OutlinedButton(onPressed: (){}, child: Text("3")),
-                  OutlinedButton(
-                    onPressed: (){}, 
-                    child: Text("+",style: TextStyle(color: Colors.white),),
-                    style: OutlinedButton.styleFrom(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(90.0),
-                      ),
-                      backgroundColor: Colors.yellow,
-                    ),
-                  ),
+                  _buildOutlinedButton("1",Colors.grey),
+                  _buildOutlinedButton("2",Colors.grey),
+                  _buildOutlinedButton("3",Colors.grey),
+                  _buildOutlinedButton("+",Colors.blue),
                 ],
               ),
                 Row(
                   mainAxisSize : MainAxisSize.min,
                 children: [
-                  OutlinedButton(onPressed: (){}, child: Text("0")),
-                  OutlinedButton(onPressed: (){}, child: Text("0")),
-                  OutlinedButton(onPressed: (){}, child: Text(".")),
-                  OutlinedButton(
-                    onPressed: (){}, 
-                    child: Text("=",style: TextStyle(color: Colors.white),),
-                    style: OutlinedButton.styleFrom(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(90.0),
-                      ),
-                      backgroundColor: Colors.yellow,
-                    ),
-                  ),
+                  _buildOutlinedButton("0",Colors.grey,width:110),
+                  _buildOutlinedButton(".",Colors.grey),
+                  _buildOutlinedButton("=",Colors.blue),
                 ],
               ),
             ],
